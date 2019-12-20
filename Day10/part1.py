71197ff190795b097568e32c6509ca5365cb0540
Chad Purdy <cpurdy@flourbluffschools.net>
2019-12-20 15:24:29 -0600

    wip

--- Day10/part1.py
+++ Day10/part1.py
@@ -16,4 +16,8 @@ for y in range(len(map)):
         if map[y][x] == '#':
             asteroids[(x,y)] = []
 
+# Need to make a list of asteroids that current asteroid can see
+# Only add an asteroid of no other asteroids have the same "slope"
 
+# Write a function blocking(atAst,ast1, ast2) that returns True
+# if ast2 is blocking ast1 from atAst.
